b7e4d17f1dbe509b7e3b06b2fce481b32977ddc8
Natasha Kushwah <84273323+Natasha-kush@users.noreply.github.com>
2024-07-02 21:15:23 +0530

    Add files via upload

---
+++ Email_generater.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class Email_generater {
+	
+	public static void main(String[]args) {
+		Email em1 = new Email("jon", "seu");
+		
+		System.out.println(em1.showInfo());
+	}
+	
+}
